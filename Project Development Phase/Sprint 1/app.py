647137a53b009101e77e63b82aff2499e2ffda32
Anu Sree <66899970+AnusreeV1@users.noreply.github.com>
2022-11-25 15:19:20 +0530

    Added flask and output screenshots

---
+++ Project Development Phase/Sprint 1/app.py
@@ -0,0 +1,57 @@
+from flask import Flask, render_template,request, redirect, url_for, session
+import ibm_db
+from flask_mail import Mail, Message
+app = Flask(__name__)
+
+@app.route('/register',methods=['POST'])
+def register():
+    x = [x for x in request.form.values()]
+    print(x)
+    name=x[0]
+    email=x[1]
+    phone=x[2]
+    city=x[3]
+    infect=x[4]
+    blood=x[5]
+    password=x[6]
+    sql = "SELECT * FROM users WHERE email =?"
+    stmt = ibm_db.prepare(conn, sql)
+    ibm_db.bind_param(stmt,1,email)
+    ibm_db.execute(stmt)
+    account = ibm_db.fetch_assoc(stmt)
+    print(account)
+    if account:
+        return render_template('index.html', pred="You are already a member, please login using your details")
+    else:
+        insert_sql = "INSERT INTO  users VALUES (?, ?, ?, ?, ?, ?, ?)"
+        prep_stmt = ibm_db.prepare(conn, insert_sql)
+        ibm_db.bind_param(prep_stmt, 1, name)
+        ibm_db.bind_param(prep_stmt, 2, email)
+        ibm_db.bind_param(prep_stmt, 3, phone)
+        ibm_db.bind_param(prep_stmt, 4, city)
+        ibm_db.bind_param(prep_stmt, 5, infect)
+        ibm_db.bind_param(prep_stmt, 6, blood)
+        ibm_db.bind_param(prep_stmt, 7, password)
+        ibm_db.execute(prep_stmt)
+        return render_template('index.html', pred="Registration Successful, please login using your details")
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
+    
+@app.route('/loginpage',methods=['POST'])
+def loginpage():
+    user = request.form['user']
+    passw = request.form['passw']
+    sql = "SELECT * FROM users WHERE email =? AND password=?"
+    stmt = ibm_db.prepare(conn, sql)
+    ibm_db.bind_param(stmt,1,user)
+    ibm_db.bind_param(stmt,2,passw)
+    ibm_db.execute(stmt)
+    account = ibm_db.fetch_assoc(stmt)
+    print (account)
+    print(user,passw)
+    if account:
+            return redirect(url_for('stats'))
+    else:
+        return render_template('login.html', pred="Login unsuccessful. Incorrect username / password !")
